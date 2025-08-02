251dc772cfc14c7a0ea86b456d2acfd225fe0869
toptuk49 <chyka228tigra@yandex.ru>
2025-08-03 02:09:41 +0300

    Set up chezmoi with zsh and neovim dotfles

---
+++ private_dot_config/nvim/lua/community.lua
@@ -0,0 +1,22 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- frontend
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  -- python
+  { import = "astrocommunity.pack.python" },
+  -- system
+  { import = "astrocommunity.pack.bash" },
+  -- import/override with your plugins folder
+}
