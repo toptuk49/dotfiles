be812c92eca058fa0161c4fa374b575311986c6f
toptuk49 <chyka228tigra@yandex.ru>
2025-08-11 16:42:03 +0300

    feat(figma): added figma cheatsheet

---
+++ private_dot_local/bin/cheatsheets-src/cheatsheets/figma.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+show_cheatsheet() {
+	cat <<EOF
+🎨 FIGMA CHEATSHEET
+
+The cheatsheet is aimed at the main way to work with figma in the context
+of frontend development.
+
+📤 Extract atoms styles
+
+If there is a need to get general styles for a page, such as fonts or colors,
+there are two working ways to get them depending on the situation.
+
+1. The layout has good architecture ✅
+
+Assets -> Local Styles
+
+2. The layout hasn't good architecture 👎
+
+- Download plugin 'Design Lint' and open it;
+- Errors List will show you all style groups you need.
+
+EOF
+}
