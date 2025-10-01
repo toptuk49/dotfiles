0cbadd9130dc77dda33af1213648b2dc32c74ee9
toptuk49 <chyka228tigra@yandex.ru>
2025-10-01 19:10:14 +0300

    feat(zsh): make mise activation script universal
    - Update mise_path.sh to auto-detect installation path
    - Support multiple installation methods: Homebrew, local bin, system
    paths
    - Improve portability across different systems and setups

--- private_dot_config/zsh/general/mise_path.sh
+++ private_dot_config/zsh/general/mise_path.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-eval "$($HOME/.local/bin/mise activate zsh)"
+if command -v mise &>/dev/null; then
+  eval "$(mise activate zsh)"
+elif [ -f "$HOME/.local/bin/mise" ]; then
+  eval "$($HOME/.local/bin/mise activate zsh)"
+elif [ -f "/opt/homebrew/bin/mise" ]; then
+  eval "$(/opt/homebrew/bin/mise activate zsh)"
+elif [ -f "/usr/local/bin/mise" ]; then
+  eval "$(/usr/local/bin/mise activate zsh)"
+fi
